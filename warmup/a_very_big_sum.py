71e00c14482e3f5f0f52b3d5ec9469af46ce64dc
SURYANSH BHARDWAJ <suryanshbhardwaj46@gmail.com>
2022-04-21 02:26:58 +0530

    Create a_very_big_sum.py

---
+++ warmup/a_very_big_sum.py
@@ -0,0 +1,19 @@
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    sumi=0
+    from functools import reduce
+    x:long = reduce(lambda x,y:x+y,ar)
+    return x
+
+if __name__ == '__main__':
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = aVeryBigSum(ar)
+    print(result)
